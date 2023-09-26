5ab93eaafd93abdf34c1bc79d4780222704927c8
Hove <d.quangtu302@gmail.com>
2023-09-26 15:50:04 +0700

    Done LocalFuntion

--- Chuong4/LocalFuntion.cs
+++ Chuong4/LocalFuntion.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,85 @@ namespace ConsoleApp1
 {
     internal class LocalFuntion
     {
+        #region Bai 1
+        /*static void Main(string[] args)
+        {
+            while (true)
+            {
+                GetData(out int a, out int b);
+                Data(a, b);
+                continue;
+            }
+        }
+        static void GetData(out int a, out int b)
+        {
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+        }
+        static void Data(int a, int b)
+        {
+            int result = 0;
+            bool Check(int n)
+            {
+                return n % 2 == 0;
+            }
+            for (int i = a; i < b; i++)
+            {
+                if (Check(i))
+                {
+                    Console.WriteLine(i);
+                    result++;
+                }
+            }
+            Console.WriteLine(result == 0 ? "ERROR" : "");
+
+        }*/
+        #endregion
+        #region Bai 2
+        /*static void Main(string[] args)
+        {
+            while (true)
+            {
+                GetData(out int a, out int b);
+                Data(a, b);
+                continue;
+            }
+        }
+        static void GetData(out int a, out int b)
+        {
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+        }
+        static void Data(int a, int b)
+        {
+            int result = 0;
+            bool Check(int n)
+            {
+                if (n < 2)
+                {
+                    return false;
+                }
+                int bound = (int)Math.Sqrt(n);
+                for (int i = 2; i <= bound; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            for (int i = a; i <= b; i++)
+            {
+                if (Check(i))
+                {
+                    Console.WriteLine(i);
+                    result++;
+                }
+            }
+            Console.WriteLine(result == 0 ? "ERROR" : "");
+
+        }*/
+        #endregion
     }
 }
